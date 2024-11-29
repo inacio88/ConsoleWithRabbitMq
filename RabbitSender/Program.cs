3f18e0da6b8314d2357fdc92f74ffe6a5184a38c
inacio88 <inacio88@outlook.com>
2024-11-29 19:49:40 -0300

    master: producer feito

--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit sender app";
+
+IConnection connection = factory.CreateConnection();
+
+IModel channnel = connection.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channnel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channnel.QueueDeclare(queueName, false, false, false, null);
+channnel.QueueBind(queueName, exchangeName, routingKey, null);
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello Mundo");
+channnel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+channnel.Close();
+
+connection.Close();
